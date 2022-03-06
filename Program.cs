f7828c557575b4fe534fd51a3b17412f70e2d324
nrgst183 <ngrst183@gmail.com>
2022-03-06 00:25:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ScreenRecorderLib;
+
+namespace RecorderThing
+{
+    public class Program
+    {
+        private static Recorder _recorder;
+        private static readonly string VideoFileDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private static readonly CancellationTokenSource CancellationTokenSource = new();
+        public static string XemuWindowSearchPattern => "xemu |";
+
+        public static bool IsRecording { get; private set; }
+        public static bool HasFinishedRecordingFile { get; private set; }
+
+        private static string VideoFileName => $"xemu {DateTime.Now:yyyy-MM-dd HH-mm-ss}.mp4";
+
+        private static async Task Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
+            try
+            {
+                await ScanAndRecordXemuProcessesAsync(CancellationTokenSource.Token);
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            CancellationTokenSource.Cancel();
+        }
+
+        private static async Task ScanAndRecordXemuProcessesAsync(CancellationToken token)
+        {
+            Console.WriteLine("Scanning for xemu processes...");
+
+            while (!token.IsCancellationRequested)
+            {
+                token.ThrowIfCancellationRequested();
+
+                switch (IsRecording)
+                {
+                    case false when HasFinishedRecordingFile:
+                        await Task.Delay(100, token);
+                        return;
+                    case true:
+                        continue;
+                }
+
+                var xemuProcess = FindXemuProcessByWindowTitle(XemuWindowSearchPattern);
+
+                if (xemuProcess != null)
+                {
+                    Console.WriteLine($"Recording {xemuProcess.MainWindowTitle}...");
+                    IsRecording = true;
+                    RecordXemuWindow(xemuProcess, VideoFileName, VideoFileDirectory);
+                }
+                else
+                {
+                    await Task.Delay(100, token);
+                }
+            }
+        }
+
+        private static void RecordXemuWindow(Process xemuProcess, string fileName, string fileDirectory)
+        {
+            var fullPath = Path.Combine(fileDirectory, fileName);
+
+            if (File.Exists(fullPath))
+                File.Delete(fullPath);
+
+            _recorder = Recorder.CreateRecorder(
+                DefaultRecorderOptions.GetDefaultRecorderOptionsFromXemuProcess(xemuProcess));
+
+            xemuProcess.EnableRaisingEvents = true;
+
+            xemuProcess.Exited += (_, _) =>
+            {
+                xemuProcess.EnableRaisingEvents = false;
+                _recorder.Stop();
+                IsRecording = false;
+                HasFinishedRecordingFile = true;
+                Console.WriteLine($"Recording for {xemuProcess.MainWindowTitle} complete");
+            };
+
+            _recorder.OnRecordingFailed += (_, e) => Console.WriteLine(e.Error);
+
+            _recorder.Record(fullPath);
+        }
+
+        private static Process FindXemuProcessByWindowTitle(string pattern)
+        {
+            return Process.GetProcesses().FirstOrDefault(process => process.MainWindowTitle.Contains(pattern));
+        }
+    }
+}
---
+++ RecorderOptions.cs
@@ -0,0 +1,62 @@
+﻿using ScreenRecorderLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorderThing
+{
+    public static class DefaultRecorderOptions
+    {
+        public static RecorderOptions GetDefaultRecorderOptionsFromXemuProcess(Process xemuProcess)
+        {
+            return new RecorderOptions
+            {
+                AudioOptions = new AudioOptions
+                {
+                    AudioInputDevice = null,
+                    AudioOutputDevice = null,
+                    IsAudioEnabled = true,
+                    IsInputDeviceEnabled = false,
+                    IsOutputDeviceEnabled = true,
+                    OutputVolume = (float?)0.8
+                },
+                VideoEncoderOptions = new VideoEncoderOptions
+                {
+                    Bitrate = 8000 * 1000,
+                    Framerate = 60,
+                    IsFixedFramerate = true,
+                    //Currently supported are H264VideoEncoder and H265VideoEncoder
+                    Encoder = new H264VideoEncoder
+                    {
+                        BitrateMode = H264BitrateControlMode.CBR,
+                        EncoderProfile = H264Profile.Main,
+                    },
+                    //Fragmented Mp4 allows playback to start at arbitrary positions inside a video stream,
+                    //instead of requiring to read the headers at the start of the stream.
+                    IsFragmentedMp4Enabled = true,
+                    //If throttling is disabled, out of memory exceptions may eventually crash the program,
+                    //depending on encoder settings and system specifications.
+                    IsThrottlingDisabled = false,
+                    //Hardware encoding is enabled by default.
+                    IsHardwareEncodingEnabled = true,
+                    //Low latency mode provides faster encoding, but can reduce quality.
+                    IsLowLatencyEnabled = true,
+                    //Fast start writes the mp4 header at the beginning of the file, to facilitate streaming.
+                    IsMp4FastStartEnabled = false
+                },
+                SourceOptions = new SourceOptions
+                {
+                    RecordingSources = new List<RecordingSourceBase>
+                    {
+                        new WindowRecordingSource(xemuProcess.MainWindowHandle)
+                    }
+                }
+            };
+        }
+    }
+}
